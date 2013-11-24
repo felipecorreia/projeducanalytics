046c656fc0d21d709c7c267fade2585a4d58f16b
Felipe <felipecorrei@gmail.com>
2013-11-24 00:25:58 -0200

    Ea_Nov

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Educ::Application.config.secret_key_base = 'f597e70d9c4ba3adf24af7a7dbe6c2895112a059766c15f7e654f5681610a6ae9629045bd9780a8a4c703db56646aea0750be12bc1251a9e764ab2a6b2446453'
